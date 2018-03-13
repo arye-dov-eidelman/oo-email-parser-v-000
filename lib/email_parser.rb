36dc430c8100d9b98783ef8e45eed68754c29201
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-13 21:19:29 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,21 +8,25 @@ class EmailParser
     @emails = emails
   end
 
-  def parse
-    if @emails.include?(',')
-      parse_by_comma
-    else if
-      parse_by_space
-    else
-    remove_duplicates
-    @emails
-  end
+  # def parse
+  #   if @emails.include?(',')
+  #     parse_by_comma
+  #   else if
+  #     parse_by_space
+  #   else
+  #   remove_duplicates
+  #   @emails
+  # end
   def parse_by_space
     @emails = @emails.split(' ').collect{ |i| i.strip}
   end
   def parse_by_comma
     @emails = @emails.split(', ').collect{ |i| i.strip}
     @emails = @emails.split(',').collect{ |i| i.strip}
+  end
+  def parse#_by_comma_and_space
+    parse_by_comma
+    parse_by_space
   end
   def remove_duplicates
     @emails = @emails.uniq
